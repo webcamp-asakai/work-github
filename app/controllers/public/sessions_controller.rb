1a20de0efbf753479bd979a1c7a8d91c2c00c2db
asakai1 <127031557+asakai1@users.noreply.github.com>
2023-05-05 22:37:37 +0000

    Fix withdraw-action

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :customer_state, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -33,5 +34,14 @@ class Public::SessionsController < Devise::SessionsController
   end
 
   protected
+
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+    if @customer
+      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == true)
+        redirect_to new_customer_registration_path
+      end
+    end
+  end
 
 end
