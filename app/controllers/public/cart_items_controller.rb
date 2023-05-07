13bdcdc08d72963ec93f7eaf853fd4549042322b
asakai1 <127031557+asakai1@users.noreply.github.com>
2023-05-07 11:00:20 +0000

    Add add-cartitem

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,11 +6,17 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @item = Item.find(cart_item_params[:item_id])
-    if Item.find_by()
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
-    @cart_item.save
-    redirect_to cart_items_path
+    if @cart_item.add_item.present?
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.save
+      redirect_to cart_items_path
+    else
+      @cart_item.save
+      redirect_to cart_items_path
+    end
   end
 
   def destroy
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -18,4 +18,8 @@ class CartItem < ApplicationRecord
   def subtotal
     item.add_tax_price * amount
   end
+
+  def add_item
+    CartItem.find_by(item_id: item.id)
+  end
 end
